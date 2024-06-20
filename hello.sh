7fe76a4bcac4ae457bee25f4f1d8595a7eeaf617
proKTheus <matheuslucasgp@gmail.com>
2024-06-20 15:17:22 -0300

    Adicionado arquivo hello.sh

---
+++ hello.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#hello-world.sh - Faz Hello World
+#
+# Criado em     : 2024/06/20
+# Atualizado		: 2024/06/20
+# Autor		      : Matheus <matheuslucasgp@gmail.com>
+#----------------------------------------
+# Notas:
+#
+#----------------------------------------
+# Exemplo:
+#
+#----------------------------------------
+# Histórico:
+#
+#----------------------------------------
+clear
+echo "Hello World"
+exit
